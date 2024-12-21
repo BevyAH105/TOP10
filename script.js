09812b36967c49c5130ded8cebbf1ad0ff96c1d6
Beverly Hensel <bevyah@outlook.com>
2024-12-21 10:44:53 -0500

    added basic validation

--- script.js
+++ script.js
@@ -24,3 +24,31 @@ window.addEventListener("scroll", function () {
   const navContainer = document.querySelector(".nav-container");
   navContainer.style.backgroundPositionY = `${-700 + scrollPosition * 0.8}px`;
 });
+
+document
+  .getElementById("signupForm")
+  .addEventListener("submit", function (event) {
+    const email = document.getElementById("email").value;
+    const password = document.getElementById("psw").value;
+    const repeatPassword = document.getElementById("psw-repeat").value;
+
+    const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
+    if (!emailPattern.test(email)) {
+      alert("Please enter a valid email address.");
+      event.preventDefault(); // Prevent form submission
+      return;
+    }
+    if (password.length < 8) {
+      alert("Password must be at least 8 characters long.");
+      event.preventDefault();
+      return;
+    }
+
+    if (password !== repeatPassword) {
+      alert("Passwords do not match.");
+      event.preventDefault();
+      return;
+    }
+
+    alert("Form submitted successfully!");
+  });
